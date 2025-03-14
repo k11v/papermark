6fda0089db5d4ececeffc2dc11ab5441c7666ad3
Kirill Gashkov <66561526+k11v@users.noreply.github.com>
2025-03-14 19:49:15 +0300

    Add hard and soft line break tests

---
+++ cmd/build/papermark_test.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestPapermark(t *testing.T) {
+	md := NewPapermark()
+
+	tests := []struct {
+		Markdown  string
+		WantTypst string
+	}{
+		// Hard line breaks
+		{Markdown: "foo  \nbaz\n", WantTypst: "foo \\\nbaz\n"},
+		{Markdown: "*foo  \nbar*\n", WantTypst: "_foo \\\nbar_\n"},
+		{Markdown: "`code  \nspan`\n", WantTypst: "`code   span`\n"},
+		{Markdown: "<a href=\"foo  \nbar\">\n", WantTypst: "\n"},
+		{Markdown: "foo  \n", WantTypst: "foo\n"},
+		{Markdown: "### foo  \n", WantTypst: "=== foo\n"},
+
+		// Soft line breaks
+		{Markdown: "foo\nbaz\n", WantTypst: "foo\nbaz\n"},
+		{Markdown: "foo \n baz\n", WantTypst: "foo\nbaz\n"},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.Markdown, func(t *testing.T) {
+			b := new(strings.Builder)
+			err := md.Convert([]byte(tt.Markdown), b)
+			if err != nil {
+				t.Fatalf("got %v err", err)
+			}
+			if got, want := b.String(), tt.WantTypst; got != want {
+				t.Fatalf("got %q, want %q", got, want)
+			}
+		})
+	}
+}
